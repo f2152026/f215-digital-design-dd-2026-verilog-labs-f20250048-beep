cf6543a50e41693c5dd896e6d882755b16e688aa
Sachinkalyan Indraganti <f20250048@pilani.bits-pilani.ac.in>
2026-08-28 21:16:16 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -8,19 +8,19 @@
 //           re-simulate with the same tb.v and compare.
 
 module FA_Gate(
-    input a,
-    input b,
-    input cin,
+    input  a,
+    input  b,
+    input  cin,
     output sum,
     output cout
 );
 
 wire ps, pc1, pc2;
 
-or  #(2) (cout, pc1, pc2);
-xor #(2) (sum, cin, ps);
-and #(2) (pc2, cin, ps);
 xor #(2) (ps, a, b);
 and #(2) (pc1, a, b);
+xor #(2) (sum, cin, ps);
+and #(2) (pc2, cin, ps);
+or  #(2) (cout, pc1, pc2);
 
 endmodule
